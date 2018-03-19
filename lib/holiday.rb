d2ab20332a57563dcfa66b06e59a71938a5881ef
Veronica Kim <vero.a.kim@gmail.com>
2018-03-19 20:28:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |key, value|
     value.each do |key, value|
-      value << supply
+      value
   end
 end
 end
